d4c40af660a96d5656d22777a771d887963285c0
Sanjay Kumar J <sanjaykumarj.pro@gmail.com>
2023-10-24 10:28:59 +0530

    conditions for file size and extension

--- controllers/upload.php
+++ controllers/upload.php
@@ -23,7 +23,7 @@ class upload {
     public function submit() {
 
         if (isset($_FILES["upload_file"]) && $_FILES["upload_file"]["error"] == 0) {
-            $allowedExtensions = array("jpg", "jpeg", "png", "gif"); // List of allowed extensions
+            $allowedExtensions = array("jpg", "pdf", "docx"); // List of allowed extensions
 
             // Get the file extension
             $fileInfo = pathinfo($_FILES["upload_file"]["name"]);
@@ -31,17 +31,25 @@ class upload {
 
             // Check if the file extension is in the list of allowed extensions
             if (in_array($fileExtension, $allowedExtensions)) {
-                $targetDir = "../uploads/";  // Directory to save the uploaded file
-                $targetFilename = time(). "_" . bin2hex(random_bytes(8)). "_" . basename($_FILES["upload_file"]["name"]);
-                $targetFile = $targetDir . $targetFilename;
 
-                $sql="INSERT INTO uploads(userid, filename) VALUES('".$_SESSION['user_id']."', '$targetFilename')";
-                $this->db->query($sql);
+                $fileSize = $_FILES["upload_file"]["size"];
+                if ($fileSize <= 5*1024*1024) {
 
-                if (move_uploaded_file($_FILES["upload_file"]["tmp_name"], $targetFile)) {
-                    $_SESSION['message'] = "The file has been uploaded successfully.";
-                } else {
-                    $_SESSION['message'] = "Sorry, there was an error uploading your file.";
+                    $targetDir = "../uploads/";  // Directory to save the uploaded file
+                    $targetFilename = time(). "_" . bin2hex(random_bytes(8)). "_" . basename($_FILES["upload_file"]["name"]);
+                    $targetFile = $targetDir . $targetFilename;
+
+                    $sql="INSERT INTO uploads(userid, filename) VALUES('".$_SESSION['user_id']."', '$targetFilename')";
+                    $this->db->query($sql);
+
+                    if (move_uploaded_file($_FILES["upload_file"]["tmp_name"], $targetFile)) {
+                        $_SESSION['message'] = "The file has been uploaded successfully.";
+                    } else {
+                        $_SESSION['message'] = "Sorry, there was an error uploading your file.";
+                    }
+                }
+                else {
+                    $_SESSION['message'] = "Please upload a file with size less than 5mb.";
                 }
             } else {
                 $_SESSION['message'] = "File extension not allowed. Please upload a file with one of the following extensions: " . implode(", ", $allowedExtensions);
@@ -50,6 +58,7 @@ class upload {
             $_SESSION['message'] = "File upload error: " . $_FILES["upload_file"]["error"];
         }
 
+        //getting the files uploaded by this user
         $sql="SELECT * FROM uploads WHERE userid='".$_SESSION['user_id']."'";
         $result=$this->db->query($sql);
         $uploaded_files=$result->fetch_all(MYSQLI_ASSOC);
